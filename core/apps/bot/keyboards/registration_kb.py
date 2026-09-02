b6a2dc599e5c5c534059c417ecbac191fddf7744
Fenix-m18 <58095963+Fenix-m18@users.noreply.github.com>
2026-09-03 00:33:46 +0300

    Translate button texts to Ukrainian

--- core/apps/bot/keyboards/registration_kb.py
+++ core/apps/bot/keyboards/registration_kb.py
@@ -1,10 +1,10 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-btn_1 = KeyboardButton('Cancel ❌')
+btn_1 = KeyboardButton('Відмінити ❌')
 markup.add(btn_1)
 
 markup_cancel_forgot_password = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-btn_1 = KeyboardButton('Cancel ❌')
-btn_2 = KeyboardButton('Forgot Password? 🆘')
+btn_1 = KeyboardButton('Відмінити ❌')
+btn_2 = KeyboardButton('Забули пароль? 🆘')
 markup_cancel_forgot_password.add(btn_1).add(btn_2)
