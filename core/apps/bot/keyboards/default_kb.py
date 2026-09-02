8d17e454c58d885d141b9f52fc5f1f4286ce5370
Fenix-m18 <58095963+Fenix-m18@users.noreply.github.com>
2026-09-02 09:34:48 +0300

    Translate keyboard buttons to Ukrainian

--- core/apps/bot/keyboards/default_kb.py
+++ core/apps/bot/keyboards/default_kb.py
@@ -1,12 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-btn_1 = KeyboardButton('Help ⭐️')
-btn_2 = KeyboardButton('Description 📌')
-btn_3 = KeyboardButton('Catalog 🛒')
-btn_4 = KeyboardButton('Admin 👑')
+btn_1 = KeyboardButton('Допомога ⭐️')
+btn_2 = KeyboardButton('Опис 📌')
+btn_3 = KeyboardButton('Каталог 🛒')
+btn_4 = KeyboardButton('Адмін 👑')
 markup.add(btn_1).insert(btn_2).add(btn_3).insert(btn_4)
 
 only_help_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-btn_1 = KeyboardButton('Help ⭐️')
+btn_1 = KeyboardButton('Допомога ⭐️')
 only_help_markup.add(btn_1)
