5450189d734557f025f4f35a2810f4153ef237c2
Fenix-m18 <58095963+Fenix-m18@users.noreply.github.com>
2026-09-02 09:39:09 +0300

    Add Cloudinary settings to production config

--- core/config/settings/prod.py
+++ core/config/settings/prod.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
 
     'core.apps.bot.apps.BotFileConfig',
     'smart_selects',
