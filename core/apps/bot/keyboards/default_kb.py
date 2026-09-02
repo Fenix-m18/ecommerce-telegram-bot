f8a0dd1950f509eb49470733b26de14eaf9ac5bd
Fenix-m18 <58095963+Fenix-m18@users.noreply.github.com>
2026-09-03 00:32:50 +0300

    Change button label from 'Опис' to 'Про нас'

--- core/apps/bot/keyboards/default_kb.py
+++ core/apps/bot/keyboards/default_kb.py
@@ -2,7 +2,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
 btn_1 = KeyboardButton('Допомога ⭐️')
-btn_2 = KeyboardButton('Опис 📌')
+btn_2 = KeyboardButton('Про нас 📌')
 btn_3 = KeyboardButton('Каталог 🛒')
 btn_4 = KeyboardButton('Адмін 👑')
 markup.add(btn_1).insert(btn_2).add(btn_3).insert(btn_4)
