c7cc61df490a776415f914a198231235359f2ee5
Fenix-m18 <58095963+Fenix-m18@users.noreply.github.com>
2026-09-03 00:34:38 +0300

    Translate sign-in/up buttons to Ukrainian

--- core/apps/bot/keyboards/sign_inup_kb.py
+++ core/apps/bot/keyboards/sign_inup_kb.py
@@ -1,7 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-btn_1 = KeyboardButton('Sign Up ✌️')
-btn_2 = KeyboardButton('Sign In 👋')
-btn_3 = KeyboardButton('Forgot Password? 🆘')
+btn_1 = KeyboardButton('Зареєструватися ✌️')
+btn_2 = KeyboardButton('Увійти 👋')
+btn_3 = KeyboardButton('Забули пароль? 🆘')
 markup.add(btn_1).insert(btn_2).add(btn_3)
