b1b3016499d26b969f985fe47884ed5132dac555
Fenix-m18 <58095963+Fenix-m18@users.noreply.github.com>
2026-09-03 00:31:37 +0300

    Change button label from 'Home' to 'Головна сторінка'

--- core/apps/bot/keyboards/admin_kb.py
+++ core/apps/bot/keyboards/admin_kb.py
@@ -1,6 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 markup = ReplyKeyboardMarkup(resize_keyboard=True)
-btn_1 = KeyboardButton("Home 🏠")
+btn_1 = KeyboardButton("Головна сторінка 🏠")
 btn_2 = KeyboardButton("Help 🔔")
 markup.add(btn_1).add(btn_2)
