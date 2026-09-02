7e492996d80941af1af6aaf9b6cf73a868a96026
Fenix-m18 <58095963+Fenix-m18@users.noreply.github.com>
2026-09-03 01:06:59 +0300

    Rename button from 'Головна сторінка' to 'Головне меню'

--- core/apps/bot/keyboards/admin_kb.py
+++ core/apps/bot/keyboards/admin_kb.py
@@ -1,6 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 markup = ReplyKeyboardMarkup(resize_keyboard=True)
-btn_1 = KeyboardButton("Головна сторінка 🏠")
+btn_1 = KeyboardButton("Головне меню 🏠")
 btn_2 = KeyboardButton("Help 🔔")
 markup.add(btn_1).add(btn_2)
