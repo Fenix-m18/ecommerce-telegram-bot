8657bff5eb4efd8924ce15060bc11894e5228f10
Fenix-m18 <58095963+Fenix-m18@users.noreply.github.com>
2026-09-03 01:10:45 +0300

    Change button text from 'Допомога' to 'Головне меню'
    Updated the button text for the main keyboard.

--- core/apps/bot/keyboards/default_kb.py
+++ core/apps/bot/keyboards/default_kb.py
@@ -1,12 +1,10 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-btn_1 = KeyboardButton('Допомога ⭐️')
 btn_2 = KeyboardButton('Про нас 📌')
 btn_3 = KeyboardButton('Каталог 🛒')
-btn_4 = KeyboardButton('Адмін 👑')
 markup.add(btn_1).insert(btn_2).add(btn_3).insert(btn_4)
 
 only_help_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-btn_1 = KeyboardButton('Допомога ⭐️')
+btn_1 = KeyboardButton('Головне меню 🏠')
 only_help_markup.add(btn_1)
